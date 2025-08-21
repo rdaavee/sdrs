2566ed2d611b9e5e88f64ea61f300b20a60dec03
Swa-ne <spaulbautista@gmail.com>
2025-08-21 15:42:54 +0800

    Added RequestReceipt schema with validation, enums, and timestamps.

---
+++ backend/src/models/request.model.ts
@@ -0,0 +1,81 @@
+import { Schema, Document, ObjectId, model } from 'mongoose';
+
+export interface RequestReceiptSchemaInterface extends Document {
+    _id: ObjectId,
+    reference_number: string,
+    student_number: string,
+    full_name: string,
+    current_address: string,
+    course: string,
+    contact_number: string,
+    email_address: string,
+    purpose_of_request: string,
+    requested_documents: string[],
+    payment_method: string,
+    paid: boolean,
+    code: string,
+    status: string,
+    createdAt: Date,
+    updatedAt: Date,
+}
+
+const RequestReceiptSchema = new Schema({
+    reference_number: {
+        type: String,
+        required: true
+    },
+    student_number: {
+        type: String,
+        required: true
+    },
+    full_name: {
+        type: String,
+        required: true
+    },
+    current_address: {
+        type: String,
+        required: true
+    },
+    course: {
+        type: String,
+        required: true
+    },
+    contact_number: {
+        type: String,
+        required: true
+    },
+    email_address: {
+        type: String,
+        required: true
+    },
+    purpose_of_request: {
+        type: String,
+        required: true
+    },
+    requested_documents: [{
+        type: String,
+        required: true
+    }],
+    payment_method: {
+        type: String,
+        enum: ["online", "cashier"],
+        required: true
+    },
+    paid: {
+        type: Boolean,
+        default: false
+    },
+    code: {
+        type: String,
+        default: false
+    },
+    status: {
+        type: String,
+        enum: ["waiting", "processing", "ready", "released"],
+        default: "waiting"
+    },
+}, {
+    timestamps: true
+});
+
+export const RequestReceipt = model<RequestReceiptSchemaInterface>("User", RequestReceiptSchema)
